4b9bb79f53ea6958afd2dc0591db0fb292b459e8
Vladislav Zavjalov <slazav@altlinux.org>
2016-03-30 14:56:34 +0300

    remove real values from the example config file

--- test_eventdb.pl
+++ test_eventdb.pl
@@ -44,25 +44,25 @@ run_test('set_alias',   '', '{"error_type":"set_alias","error_message":"wrong nu
 
 # login test users (vk, fb, lj)
 # first user gets admin rights
-my $o1 = run_test('login', '382512edfa7149b79b910cf6227e3e16', '{"identity": "http://slazav.livejournal.com/", "provider": "lj", "full_name": "slazav", "alias": "slazav@lj", "level": "admin", "session": "-", "stime": 1234567890}');
-my $o2 = run_test('login', '7202c11c442dbd1e7c7f9c33e2ee61d9', '{"identity": "https://www.facebook.com/app_scoped_user_id/100004349705357/", "provider": "fb", "full_name": "Zavjalov Vladislav", "alias": "ZavjalovVladislav@fb", "level": "norm", "session": "-", "stime": 1234567890}');
-my $o3 = run_test('login', '6222d12c54a233deae789c3ce22eb1d9', '{"identity": "http://vk.com/id322772569", "provider": "vk", "full_name": "Zavjalov Vladislav", "alias": "ZavjalovVladislav@vk", "level": "norm", "session": "-", "stime": 1234567890}');
+my $o1 = run_test('login', '382512edfa7149b79b910cf6227e3e16', '{"identity": "http://test.livejournal.com/", "provider": "lj", "full_name": "test", "alias": "test@lj", "level": "admin", "session": "-", "stime": 1234567890}');
+my $o2 = run_test('login', '7202c11c442dbd1e7c7f9c33e2ee61d9', '{"identity": "https://www.facebook.com/app_scoped_user_id/000000000000000/", "provider": "fb", "full_name": "Test User", "alias": "TestUser@fb", "level": "norm", "session": "-", "stime": 1234567890}');
+my $o3 = run_test('login', '6222d12c54a233deae789c3ce22eb1d9', '{"identity": "http://vk.com/id000000000", "provider": "vk", "full_name": "Test User", "alias": "TestUser@vk", "level": "norm", "session": "-", "stime": 1234567890}');
 
 # login again
-my $o4 = run_test('login', '6222d12c54a233deae789c3ce22eb1d9', '{"identity": "http://vk.com/id322772569", "provider": "vk", "full_name": "Zavjalov Vladislav", "alias": "ZavjalovVladislav@vk", "level": "norm", "session": "-", "stime": 1234567890}');
+my $o4 = run_test('login', '6222d12c54a233deae789c3ce22eb1d9', '{"identity": "http://vk.com/id000000000", "provider": "vk", "full_name": "Test User", "alias": "TestUser@vk", "level": "norm", "session": "-", "stime": 1234567890}');
 
 # user_info  (o3 session is not valid)
-run_test('user_info', $o1->{session}, '{"identity": "http://slazav.livejournal.com/", "provider": "lj", "full_name": "slazav", "alias": "slazav@lj", "level": "admin", "session": "-", "stime": 1234567890}');
-run_test('user_info', $o2->{session}, '{"identity": "https://www.facebook.com/app_scoped_user_id/100004349705357/", "provider": "fb", "full_name": "Zavjalov Vladislav", "alias": "ZavjalovVladislav@fb", "level": "norm", "session": "-", "stime": 1234567890}');
+run_test('user_info', $o1->{session}, '{"identity": "http://test.livejournal.com/", "provider": "lj", "full_name": "test", "alias": "test@lj", "level": "admin", "session": "-", "stime": 1234567890}');
+run_test('user_info', $o2->{session}, '{"identity": "https://www.facebook.com/app_scoped_user_id/000000000000000/", "provider": "fb", "full_name": "Test User", "alias": "TestUser@fb", "level": "norm", "session": "-", "stime": 1234567890}');
 run_test('user_info', $o3->{session}, '{"error_type": "jsondb", "error_message":"login error"}');
-run_test('user_info', $o4->{session}, '{"identity": "http://vk.com/id322772569", "provider": "vk", "full_name": "Zavjalov Vladislav", "alias": "ZavjalovVladislav@vk", "level": "norm", "session": "-", "stime": 1234567890}');
+run_test('user_info', $o4->{session}, '{"identity": "http://vk.com/id000000000", "provider": "vk", "full_name": "Test User", "alias": "TestUser@vk", "level": "norm", "session": "-", "stime": 1234567890}');
 
 # logout
-run_test('logout', $o2->{session}, '{"identity": "https://www.facebook.com/app_scoped_user_id/100004349705357/", "provider": "fb", "full_name": "Zavjalov Vladislav", "alias": "ZavjalovVladislav@fb", "level": "norm", "session": "", "stime": 1234567890}');
+run_test('logout', $o2->{session}, '{"identity": "https://www.facebook.com/app_scoped_user_id/000000000000000/", "provider": "fb", "full_name": "Test User", "alias": "TestUser@fb", "level": "norm", "session": "", "stime": 1234567890}');
 run_test('user_info', $o2->{session}, '{"error_type": "jsondb", "error_message":"login error"}');
 
 # set alias
-run_test('set_alias sla', $o1->{session}, '{"identity": "http://slazav.livejournal.com/", "provider": "lj", "full_name": "slazav", "alias": "sla", "level": "admin", "session": "-", "stime": 1234567890}');
+run_test('set_alias sla', $o1->{session}, '{"identity": "http://test.livejournal.com/", "provider": "lj", "full_name": "test", "alias": "sla", "level": "admin", "session": "-", "stime": 1234567890}');
 
 # aliases are unique (this also causes an error message from bercleydb to stderr)
 run_test('set_alias sla', $o4->{session}, '{"error_type": "jsondb", "error_message":"./data/user.db: Invalid argument"}');
