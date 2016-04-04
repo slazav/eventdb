3355e36af9f9897b07b830b734d0f10b6e61f638
Vladislav Zavjalov <slazav@altlinux.org>
2016-04-04 11:47:43 +0300

    add www files: login/logout

---
+++ www/auth3s.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+use strict;
+use CGI   ':standard';
+use HTTP::Tiny;
+use JSON;
+
+my $evdb_prog="/usr/home/slazav/eventdb/eventdb";
+my $providers="livejournal,facebook,vkontakte,yandex,google";
+my $url="http://slazav.mccme.ru/perl/auth3.pl";
+
+my $token   = param('token') || '';
+my $session = param('session') || cookie('SESSION') || '';
+my $action  = param('action') || '';
+my $ret     = cookie('RETPAGE') || '';
+
+my $out;
+my $cookie;
+
+if ($action eq ''){
+  $out=qx(printf "%s" "$token" | $evdb_prog login 2>&1) || '';
+  # new session
+  my $data  = decode_json($out);
+  $session = $data->{session} || '';
+  $cookie = cookie(-name=>'SESSION', -value=>$session,
+                   -expires=>'+1y', -domain=>'slazav.mccme.ru') if $session;
+  if ($ret){
+    print redirect (-uri=>$ret, -cookie=>$cookie);
+    exit 0;
+  }
+}
+
+elsif ($action eq 'logout'){
+  $out=qx(printf "%s" "$session" | $evdb_prog logout 2>&1) || '';
+  $cookie = cookie(-name=>'SESSION',
+                   -expires=>'-1y', -domain=>'slazav.mccme.ru');
+}
+
+elsif ($action eq 'my_info'){
+  $out=qx(printf "%s" "$session" | $evdb_prog my_info 2>&1) || '';
+}
+
+else{
+  $out='{"error_type": "eventdb", "error_message": "Unknown action"}';
+}
+
+if ($cookie){
+  print header (-type=>'application/json', -charset=>'utf-8', -cookie=>$cookie); }
+else{
+  print header (-type=>'application/json', -charset=>'utf-8'); }
+
+print $out;
+
